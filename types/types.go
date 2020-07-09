05cbcee012e3d761497042132bdbbc5a2703ec51
Marco Peereboom <marco@peereboom.us>
2020-07-09 12:41:35 -0500

    Doco

--- types/types.go
+++ types/types.go
@@ -10,18 +10,19 @@ const (
 	PCVersion = 1 // Protocol version.
 
 	// Command identifiers
-	PCCmd                 = "cmd" // Generic encapsulating command
-	PCAck                 = "ack" // Acknowledge command
-	PCErrorCmd            = "error"
-	PCCollectOnceCmd      = "collectonce"
-	PCCollectOnceReplyCmd = "collectoncereply"
-	PCStartCollectionCmd  = "startcollection"
-	PCStopCollectionCmd   = "stopcollection"
-	PCRegisterSink        = "registersink"
+	PCCmd                 = "cmd"              // Generic encapsulating command
+	PCAck                 = "ack"              // Acknowledge command
+	PCErrorCmd            = "error"            // Error reply to a command
+	PCCollectOnceCmd      = "collectonce"      // Collect one measurement
+	PCCollectOnceReplyCmd = "collectoncereply" // Reply to collect once
+	PCStartCollectionCmd  = "startcollection"  // Start collecting measurements
+	PCStopCollectionCmd   = "stopcollection"   // Stop collecting measurements
+	PCRegisterSink        = "registersink"     // Register a sink
 
 	PCChannel = "collector" // SSH channel name
 )
 
+// PCCommand encapsulates commands with a version and a tag.
 type PCCommand struct {
 	Version uint   // Protocol version
 	Tag     uint   // Tag of command
@@ -63,6 +64,7 @@ type PCCollectionStatus struct {
 	Systems   []string  // Systems that are being polled
 }
 
+// PCCollection is a raw measurement that is sunk into the network.
 type PCCollection struct {
 	Timestamp   time.Time     // Time of collection
 	Duration    time.Duration // Time collection took
@@ -70,6 +72,8 @@ type PCCollection struct {
 	Measurement []byte        // Raw measurement
 }
 
+// Encode encodes an interface with gob. This should only be called with types
+// in this file.
 func Encode(x interface{}) ([]byte, error) {
 	var blob bytes.Buffer
 	enc := gob.NewEncoder(&blob)
@@ -80,6 +84,8 @@ func Encode(x interface{}) ([]byte, error) {
 	return blob.Bytes(), nil
 }
 
+// Decode decodes an interface with gob. This should only be called with types
+// in this file.
 func Decode(t string, blob []byte) (interface{}, error) {
 	var s PCCommand
 	dec := gob.NewDecoder(bytes.NewReader(blob))
@@ -90,6 +96,7 @@ func Decode(t string, blob []byte) (interface{}, error) {
 	return s, err
 }
 
+// init registers all gob types.
 func init() {
 	gob.Register(PCError{})
 	gob.Register(PCCollectOnce{})
